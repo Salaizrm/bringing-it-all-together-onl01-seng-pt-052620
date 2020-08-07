7c7af32e53676c908a6c3451059007ae3816a708
Robert Salaiz <work.salaizrm@gmail.com>
2020-08-07 00:08:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -32,7 +32,8 @@ class Dog
       VALUES (?, ?)
     SQL
 
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql, self.name, self.breed)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
   end
 
 end
