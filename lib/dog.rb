dd88f2840fdf814bd788c02b182d9044307e8a8a
Robert Salaiz <work.salaizrm@gmail.com>
2020-08-07 00:03:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,9 +10,14 @@ class Dog
 
   def self.create_table
     sql = <<-SQL
-
+      CREATE TABLE dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      );
     SQL
 
+    DB[:conn].execute(sql)
   end
 
 end
